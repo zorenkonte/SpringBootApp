ce243db77c9928fb82c84a5093f548897f59a255
darkmodeee <bringino.renzo36@gmail.com>
2020-09-11 19:13:41 +0800

    enable http tracing and log auditing

---
+++ src/main/java/com/dark/mode/springhibernate/cofig/AppConfiguration.java
@@ -0,0 +1,23 @@
+package com.dark.mode.springhibernate.cofig;
+
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
+    @Bean
+    public AuditEventRepository auditEventRepository() {
+        return new InMemoryAuditEventRepository();
+    }
+
+}
