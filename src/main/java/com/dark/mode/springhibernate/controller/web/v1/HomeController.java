d6f191d90516668667f5b3885fd50074b93f6ec9
darkmodeee <bringino.renzo36@gmail.com>
2020-09-11 14:24:22 +0800

    override banner.txt to make me feel special

---
+++ src/main/java/com/dark/mode/springhibernate/controller/web/v1/HomeController.java
@@ -0,0 +1,18 @@
+package com.dark.mode.springhibernate.controller.web.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String home() {
+        return "home";
+    }
+
+}
